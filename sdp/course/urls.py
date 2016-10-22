366edf25b2fb0788d608fa9db491ae8f7f70c8df
irsisyphus <cgao96@gmail.com>
2016-10-22 21:45:57 +0800

    add gitignore

--- sdp/course/urls.py
+++ sdp/course/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.all_courses, name='all_courses'),
-    url(r'^(?P<course_id>\d+)$', views.course_info, name='course_info'),
+    url(r'^course/(?P<course_id>\d+)$', views.course_info, name='course_info'),
 ]
--- sdp/sdp/urls.py
+++ sdp/sdp/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 urlpatterns = [
-    url(r'^course/', include('course.urls')),
+    url(r'^', include('course.urls')),
     url(r'^admin/', admin.site.urls),
 ]
