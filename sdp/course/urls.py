c8fabbefafef40f1dda883e2ffc0a32b8514c0c7
irsisyphus <cgao96@gmail.com>
2016-10-22 22:13:35 +0800

    add \'include\' in urls.py

--- sdp/course/urls.py
+++ sdp/course/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 urlpatterns = [
     url(r'^$', views.all_courses, name='all_courses'),
-    url(r'^course/(?P<course_id>\d+)$', views.course_info, name='course_info'),
+    url(r'^course/',include([
+        url(r'^(?P<course_id>\d+)$',views.course_info, name='course_info'),
+    ])),
 ]
