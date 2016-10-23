0cfd1a792b919e098336af3bfa4d62fcc2beffcd
irsisyphus <cgao96@gmail.com>
2016-10-23 15:12:18 +0800

    second css commit

--- sdp/course/urls.py
+++ sdp/course/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from . import views
 urlpatterns = [
-    url(r'^$', views.all_courses, name='all_courses'),
+    url(r'^$', views.index, name='index'),
     url(r'^course/',include([
         url(r'^(?P<course_id>\d+)$',views.course_info, name='course_info'),
+    ])),
+    url(r'^catagory/',include([
+        url(r'^(?P<catagory_id>\d+)$',views.catagory_info, name='catagory_info'),
     ])),
 ]
--- sdp/course/views.py
+++ sdp/course/views.py
@@ -4,6 +4,11 @@ from .models import Catagory
 from .models import Course
 from django.shortcuts import render_to_response
 
+def index(request):
+    catagories = Catagory.objects.all()
+    title = "Welcome to AB Credit (HK) Staff Development Platform!"
+    return render_to_response('index.html', locals())
+
 def course_info(request, course_id):
     course = Course.objects.get(pk=course_id)
     title = course.name
@@ -12,10 +17,7 @@ def course_info(request, course_id):
     description = course.description
     return render_to_response('course_info.html', locals())
 
-def all_courses(request):
-    menu = dict()
-    for catagory in Catagory.objects.all():
-        menu[catagory.name] = list()
-    for course in Course.objects.filter(is_open = True):
-        menu[course.catagory.name].append(course)
-    return render_to_response('index.html', locals())
+def catagory_info(request, catagory_id):
+    parent_catagory = Catagory.objects.get(pk=catagory_id)
+    courses = Course.objects.filter(catagory = parent_catagory, is_open = True)
+    return render_to_response('catagory_info.html', locals())
