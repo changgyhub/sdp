a3bd1072f58582cb6998de0c79f1d902cf7008f6
irsisyphus <cgao96@gmail.com>
2016-10-25 15:14:09 +0800

    change css

--- sdp/course/urls.py
+++ sdp/course/urls.py
@@ -3,10 +3,13 @@ from . import views
 urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^logout/', views.logout, name='logout'),
-    url(r'^course/',include([
-        url(r'^(?P<course_id>\d+)$',views.course_info, name='course_info'),
-    ])),
-    url(r'^catagory/',include([
-        url(r'^(?P<catagory_id>\d+)$',views.catagory_info, name='catagory_info'),
+    url(r'^instructor/',include([
+        url(r'^course/',include([
+            url(r'^$', views.instructor_course, name='instructor_course'),
+            url(r'^(?P<course_id>\d+)$',views.instructor_course_info, name='instructor_course_info'),
+        ])),
+        url(r'^catagory/',include([
+            url(r'^(?P<catagory_id>\d+)$',views.instructor_catagory_info, name='instructor_catagory_info'),
+        ])),
     ])),
 ]
--- sdp/course/views.py
+++ sdp/course/views.py
@@ -42,27 +42,26 @@ def logout(request):
     auth.logout(request)
     return HttpResponseRedirect("/")
 
-def course_info(request, course_id):
+def instructor_course_info(request, course_id):
     if request.user.is_authenticated():
         course = Course.objects.get(pk=course_id)
         title = course.name
         catagory = course.catagory.name
         instructor = course.instructor.name
         description = course.description
-        return render_to_response('course_info.html', locals())
+        return render_to_response('instructor/course_info.html', locals())
     else:
         return HttpResponseRedirect("/")
 
-def catagory_info(request, catagory_id):
+def instructor_catagory_info(request, catagory_id):
     if request.user.is_authenticated():
         parent_catagory = Catagory.objects.get(pk=catagory_id)
         courses = Course.objects.filter(catagory = parent_catagory, is_open = True)
-        return render_to_response('catagory_info.html', locals())
+        return render_to_response('instructor/catagory_info.html', locals())
     else:
         return HttpResponseRedirect("/")
 
 def instrcutor_index(request):
-    catagories = Catagory.objects.all()
     hour = dt.datetime.now().hour
     if hour < 6 or hour > 19:
         title = "Good night, "
@@ -75,4 +74,11 @@ def instrcutor_index(request):
     instructor = Instructor.objects.get(pk=request.user.id)
     title += instructor.name + "!"
     content = "Daily Notices:"
-    return render_to_response('test.html', locals())
+    return render_to_response('instructor/index.html', locals())
+
+def instructor_course(request):
+    if request.user.is_authenticated():
+        catagories = Catagory.objects.all()
+        return render_to_response('instructor/course.html', locals())
+    else:
+        return HttpResponseRedirect("/")
